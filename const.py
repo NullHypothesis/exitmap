75ea5395e2d028a4f94161a24591d54df530f6ea
Philipp Winter <identity.function@gmail.com>
2013-11-24 20:39:23 +0100

    Make "FIRST_HOP" unset by default.
    The constant should by set individually by whoever is running the scanner.  We
    don't want a central point of observation/failure.

--- const.py
+++ const.py
@@ -1,7 +1,8 @@
 TOOL_NAME = "scanner"
 
 # Entry guard/middle relay used as only hop before the respective exit relay.
-FIRST_HOP = "5D0034A368E0ABAF663D21847E1C9B6CFA09752A"
+# FIXME: Add your relay's fingerprint here.
+FIRST_HOP = ""
 
 CIRCUIT_POOL_SIZE = 20
 
