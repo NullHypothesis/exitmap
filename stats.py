6bc056d9b61bec1c04b14c7902bac45ecfebad7f
Philipp Winter <phw@nymity.ch>
2013-12-06 10:41:31 +0100

    Add a statistics class.
    It's used throughout exitmap to keep track of scanning statistics.  Before the
    program terminates, the statistics are shown.

---
+++ stats.py
@@ -0,0 +1,25 @@
+import log
+
+logger = log.getLogger()
+
+class Statistics( object ):
+
+    def __init__( self ):
+        """
+        Initialise a Statistics object.
+        """
+
+        self.totalCircuits = 0
+        self.failedCircuits = 0
+        self.modulesRun = 0
+
+    def __str__( self ):
+        """
+        Print the gathered statistics.
+        """
+
+        ret = "Determining scan statistics.\n"
+        ret += "Ran %d modules.\n" % self.modulesRun
+        ret += "%d of %d circuits failed." % (self.failedCircuits,
+                                              self.totalCircuits)
+        return ret
