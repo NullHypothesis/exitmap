751fbe6bd1ff888c10307f6ad4ca1cce81a073bd
Philipp Winter <phw@nymity.ch>
2014-01-09 23:47:26 +0100

    Probe several domains at once.
    Outsource the probing functionality to a dedicated function and create a
    dictionary which holds all the domains and corresponding IPv4 addresses to
    probe.

--- modules/dns.py
+++ modules/dns.py
@@ -12,25 +12,37 @@ logger = log.getLogger()
 
 destinations = None
 
-def probe( exitFpr, cmd ):
+def resolve( exitFpr, domain, whitelist ):
     """
-    Probe the given exit relay and check if the resolved domain is as expected.
-    """
+    Resolve a `domain' and compare it to the `whitelist'.
 
-    whitelist = [
-        "38.229.72.14",
-        "93.95.227.222",
-        "86.59.30.40",
-        "38.229.72.16",
-        "82.195.75.101"
-    ]
+    If the domain is not part of the whitelist, an error is logged.
+    """
 
     sock = mysocks.socksocket()
     sock.setproxy(mysocks.PROXY_TYPE_SOCKS5, "127.0.0.1", const.TOR_SOCKS_PORT)
-    ipv4 = sock.resolve("www.torproject.org")
+    # Resolve the domain using Tor's SOCKS extension.
+    ipv4 = sock.resolve(domain)
 
     if ipv4 not in whitelist:
-        logger.error("Exit relay %s returned unexpected IPv4 address: %s" %
-                     (exitFpr, ipv4))
+        logger.error("Exit relay %s returned unexpected IPv4 address for " \
+                     "\"%s\": %s." % (exitFpr, domain, ipv4))
     else:
-        logger.info("IPv4 address for %s as expected: %s" % (exitFpr, ipv4))
+        logger.info("IPv4 address of \"%s\" as expected for %s." %
+                    (domain, exitFpr))
+
+def probe( exitFpr, cmd ):
+    """
+    Probe the given exit relay and check if the domains resolve as expected.
+    """
+
+    # Format: <domain> : <ipv4_addresses>
+    domains = {
+        "youporn.com" : ["31.192.116.24"],
+        "www.torproject.org" : ["38.229.72.14", "93.95.227.222", "86.59.30.40",
+                                "38.229.72.16", "82.195.75.101"],
+        "www.wikileaks.org" : ["95.211.113.131", "95.211.113.154"]
+    }
+
+    for domain in domains.iterkeys():
+        resolve(exitFpr, domain, domains[domain])
